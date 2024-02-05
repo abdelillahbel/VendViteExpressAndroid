3e1c722673232c95cdec5665dc79e1d3cecdb61c
abdelillahbel <abdelillahbel@engineer.com>
2024-02-05 23:40:15 +0100

    Setup style on main activity

---
+++ app/src/main/java/com/ensb/vendviteexpress/view/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.ensb.vendviteexpress.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ensb.vendviteexpress.R
+import com.ensb.vendviteexpress.view.ui.account.AccountFragment
+import com.ensb.vendviteexpress.view.ui.home.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        loadFragment(HomeFragment())
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+
+                R.id.accountFragment -> {
+                    loadFragment(AccountFragment())
+                    true
+                }
+
+
+                else -> throw AssertionError()
+
+
+            }
+        }
+    }
+
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, fragment)
+        transaction.commit()
+    }
+}
