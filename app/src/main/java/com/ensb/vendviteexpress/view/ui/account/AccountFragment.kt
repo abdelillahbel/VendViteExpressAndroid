0dfda5d9b1135df0538389cd9f390a85af2d9dcd
abdelillahbel <abdelillahbel@engineer.com>
2024-02-05 23:53:18 +0100

    Code clean up

--- app/src/main/java/com/ensb/vendviteexpress/view/ui/account/AccountFragment.kt
+++ app/src/main/java/com/ensb/vendviteexpress/view/ui/account/AccountFragment.kt
@@ -10,12 +10,10 @@ import com.ensb.vendviteexpress.R
 
 class AccountFragment : Fragment() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
