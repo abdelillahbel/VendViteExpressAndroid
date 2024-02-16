31bb6e3fd8c4157c74fda9e909035c21eb60d8e3
abdelillahbel <abdelillahbel@engineer.com>
2024-02-17 00:38:29 +0100

    Add features

---
+++ app/src/main/java/com/ensb/vendviteexpress/entities/Product.kt
@@ -0,0 +1,8 @@
+package com.ensb.vendviteexpress.entities
+
+data class Product(
+    val productName: String,
+    val productId: String,
+    val description: String,
+    val ownerUid: String
+)
--- app/src/main/java/com/ensb/vendviteexpress/entities/User.kt
+++ app/src/main/java/com/ensb/vendviteexpress/entities/User.kt
@@ -5,7 +5,8 @@ import com.google.firebase.firestore.GeoPoint
 data class User(
     val name: String,
     val email: String,
+    val phoneNumber: String? = null,
     val type: String,
-   // val location: GeoPoint,
+    val location: GeoPoint?,
     val uid: String? = null // Firebase assigns the user ID on auth creation
 )
