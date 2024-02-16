90128a453c56c4ed95a8c1cac29380916ff79f53
abdelillahbel <abdelillahbel@engineer.com>
2024-02-17 00:37:24 +0100

    Add features

--- app/src/main/java/com/ensb/vendviteexpress/utils/Utils.kt
+++ app/src/main/java/com/ensb/vendviteexpress/utils/Utils.kt
@@ -1,16 +1,59 @@
 package com.ensb.vendviteexpress.utils
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.text.TextUtils
 import android.util.Patterns
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import com.ensb.vendviteexpress.view.ui.seller.SellerActivity
 import com.ensb.vendviteexpress.view.ui.auth.AuthActivity
+import java.util.Calendar
 
 object Utils {
+
+
+    const val LOCATION_PERMISSION_CODE = 100
+
+    fun checkLocationPermission(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestLocationPermission(fragment: Fragment) {
+        ActivityCompat.requestPermissions(
+            fragment.requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_CODE
+        )
+    }
+
+    // This might go into the Fragment, as it handles the result
+    fun isPermissionGranted(requestCode: Int, grantResults: IntArray): Boolean {
+        return if (requestCode == LOCATION_PERMISSION_CODE && grantResults.isNotEmpty()) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+    }
+
+    fun currentDate(): Calendar {
+        return Calendar.getInstance()
+    }
+
 
     fun NavController.safeNavigate(direction: Any) {
         val action = if (direction is NavDirections) direction.actionId else direction
@@ -48,6 +91,19 @@ object Utils {
 
     fun String.isEmailValid(): Boolean {
         return !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
